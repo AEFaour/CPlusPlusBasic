ae2c55cb2370e7fbe01eb6c2d2acb39e9e1db604
AEFaour <44943887+AEFaour@users.noreply.github.com>
2022-10-17 21:57:10 +0200

    update

--- Le Juste Prix/main.cpp
+++ Le Juste Prix/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+using namespace std;
+
 int main () {
-
-std::cout << 5;
-
+    // le commentaire est grise
+    /* un commentaire
+    sur plusieurs lignes */
+    cout << "Bienvenue au Juste prix" << endl;
+    int nomComposee{3'000'000}, nomComposee2{1'000};
+    double nombreAVirgule{3.022};
+    cout << nomComposee << endl << nomComposee2 << endl << nombreAVirgule;
 }
+
